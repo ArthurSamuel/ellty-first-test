d106412bc7ff222e4c864f530b500bb20ae38495
Arth <arthursamuel1696@gmail.com>
2026-04-25 17:46:19 +0800

    first init

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./App.css";
+import Selector from "./component/Selector";
+import Button from "./component/button";
+
+function App() {
+  const [allChecked, setAllChecked] = useState(false);
+  const [checked, setChecked] = useState(Array(6).fill(undefined));
+
+  const handleOnChange = (index: number) => {
+    setChecked((prev) =>
+      prev.map((item, indexItem) => {
+        if (index === indexItem) {
+          item = !item;
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleOnChangeAll = () => {
+    setChecked((prev) =>
+      prev.map((item) => {
+        item = !allChecked;
+        return item;
+      })
+    );
+    setAllChecked((prev) => !prev);
+  };
+
+  return (
+    <div className="card">
+      <Selector
+        checked={allChecked}
+        label="All pages"
+        onChange={() => handleOnChangeAll()}
+      />
+      <div className="gutter">
+        <div className="line"></div>
+      </div>
+      <div className="wrapper">
+        {checked.map((item, index) => (
+          <Selector
+            key={index}
+            checked={item}
+            label={`Page ${index + 1}`}
+            onChange={() => handleOnChange(index)}
+          />
+        ))}
+      </div>
+      <div className="gutter">
+        <div className="line"></div>
+      </div>
+      <div className="buttonContainer">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Selector.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import "./Selector.css";
+
+interface SelectorProps {
+  label: string;
+  checked: boolean;
+  onChange: (next: boolean) => void;
+}
+
+export default function Selector({ label, checked, onChange }: SelectorProps) {
+  const [state, setState] = useState("inactive");
+  const [arm, setArmed] = useState(false);
+
+  useEffect(() => {
+    if (checked) {
+      setState("active");
+    } else {
+      setState("exit");
+      setArmed(false);
+      const t = setTimeout(() => {
+        setState("inactive");
+      }, 250);
+      return () => clearTimeout(t);
+    }
+  }, [checked]);
+
+  function handleClick() {
+    onChange(!checked);
+  }
+
+  return (
+    <div
+      className="container"
+      onClick={handleClick}
+      onMouseLeave={() => {
+        if (!arm && state === "active") setArmed(true);
+      }}
+    >
+      <div className="checkboxLabel">{label}</div>
+
+      <div className={`checkbox ${state} ${arm ? "arm" : ""}`}>
+        <svg
+          className="checkboxIcon"
+          width="17"
+          height="13"
+          viewBox="0 0 17 13"
+          fill="none"
+        >
+          <path
+            d="M0.5 6.572L6.04879 11.5072C6.06925 11.5254 6.10055 11.5237 6.11899 11.5035L16.14 0.5"
+            stroke="currentColor"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
